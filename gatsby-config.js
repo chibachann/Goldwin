041f64b7e13b291e343ff07d2ed38b007b82bbfc
Chiba <ryotai2677@gmail.com>
2024-02-24 14:40:55 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `goldwin-web-store`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
